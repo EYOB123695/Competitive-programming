b3e5c7de8dcd63ecf84cf843dcbf49481ca8c909
EYOB123695 <116643686+EYOB123695@users.noreply.github.com>
2023-01-09 10:39:01 -0800

    Create Fizzz buzz.cpp

---
+++ Fizzz buzz.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+         vector<string> a(n , "" ) ;
+         for (int i=1 ; i <n+1 ;i++) {
+             if (i%15==0) 
+                 a[i-1]="FizzBuzz" ;
+             
+           else if (i%5==0)   a[i-1]="Buzz" ;
+           
+            else if(i%3==0)  a[i-1]="Fizz" ;
+           
+           else 
+               a[i-1] =to_string (i);
+               
+           }
+           return a  ; 
+         } 
+       
+    
+};
