4ed9f8f4dad0862a573905ed8bb98198e14a5f18
Eyob <eyobimpactinglife@gmail.com>
2024-02-27 10:37:36 -0800

    Add solution for Valid Palindrome

---
+++ leetcode/valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+
+        return True
+
+ 
+        
+
+        
