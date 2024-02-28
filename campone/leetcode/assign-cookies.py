1ce069376e1907d59b4670047bd84952ecf09c3e
Eyob <eyobimpactinglife@gmail.com>
2024-02-28 01:50:25 -0800

    Add solution for Assign Cookies

---
+++ campone/leetcode/assign-cookies.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        content=0
+        r=0
+        l=0
+        
+        g.sort()
+        s.sort()
+        while r < len(g) and l < len(s):
+            if  s[l] >= g[r] :
+                content+=1
+                r+=1
+            
+            l+=1
+        return content 
+
+
+
+
+
+        
