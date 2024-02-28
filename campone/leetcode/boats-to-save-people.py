0776960ee06e63df5c109dc5e0bb09042f43dc23
Eyob <eyobimpactinglife@gmail.com>
2024-02-28 01:55:27 -0800

    Add solution for Boats to Save People

---
+++ campone/leetcode/boats-to-save-people.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+        left = 0
+        right = len(people)-1
+        count=0
+        while left <= right :
+            count+=1
+            
+
+            if people[left] + people[right] <= limit  :
+                left+=1
+            right-=1
+                
+        return count
+            
+             
+
+
+
+        
