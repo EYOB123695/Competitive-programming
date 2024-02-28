ba204623eb900e239d60f7e3b05521f0ce0883d6
Eyob <eyobimpactinglife@gmail.com>
2024-02-28 07:29:18 -0800

    Add solution for Range Sum Query - Immutable

---
+++ campone/leetcode/range-sum-query-immutable.py
@@ -0,0 +1,19 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefixsum = [0]
+        for num in nums:
+            self.prefixsum.append(self.prefixsum[-1] + num)
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        left=self.prefixsum[left]
+        right=self.prefixsum[right+1]
+        ans=right-left
+        return ans
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
