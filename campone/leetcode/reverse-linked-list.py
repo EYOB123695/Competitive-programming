3eaa3f3e6b065f8c8891deac73e9b8a9e3ec6b81
Eyob <eyobimpactinglife@gmail.com>
2024-02-29 06:45:31 -0800

    Add solution for Reverse Linked List

---
+++ campone/leetcode/reverse-linked-list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = None 
+        fast = head 
+        while fast :  
+            tmp = fast.next 
+            fast.next = slow
+            slow = fast
+            fast = tmp
+        return slow
+        
+
+       
+
+
+
+
+
+        
+        
