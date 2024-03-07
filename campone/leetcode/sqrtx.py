9d821458260648372e4262c18f451167ade6bff7
Eyob <eyobimpactinglife@gmail.com>
2024-03-07 03:27:40 -0800

    Add solution for Sqrt(x)

---
+++ campone/leetcode/sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return x
+        
+        l,h=1,x
+        while l<=h:
+            mid=(l+h)//2
+            
+            if mid*mid==x:
+                return mid
+            elif mid * mid > x:
+                h = mid -1
+            else:
+                l= mid+1
+        return h
+
+        
