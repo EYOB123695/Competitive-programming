1b7779a46c927e1f0102ab1048bdc0c6a783e782
Eyob <eyobimpactinglife@gmail.com>
2024-03-08 07:20:41 -0800

    Add solution for Maximum Average Subarray I

---
+++ campone/leetcode/maximum-average-subarray-i.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        average = 0
+        sum = 0 
+        maxaverage=float('-inf')
+        ans = 0
+        l = 0
+        r = k
+
+        prefixsum=[0] * (len(nums)+1)
+
+
+        for i in range(len(nums)):
+             
+            prefixsum[i+1]  = prefixsum[i] + nums[i] 
+        
+        while r < len(prefixsum) :
+        
+            
+            sum = prefixsum[r] - prefixsum[r-k]
+             
+            maxaverage =max(sum,maxaverage) 
+            r+=1
+            
+        
+        
+        return maxaverage / k
+       
+
+        
