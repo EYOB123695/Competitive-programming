8d648d8a0e24857fcd2ca77c1fea3e5eb5b3ebd8
Eyob <eyobimpactinglife@gmail.com>
2024-03-07 21:44:42 -0800

    Add solution for Lemonade Change

---
+++ campone/leetcode/lemonade-change.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives = 0
+        tens = 0
+
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+            elif bill == 10:
+                if fives == 0:
+                    return False
+                fives -= 1
+                tens += 1
+            elif bill == 20:
+                if tens > 0 and fives > 0:
+                    tens -= 1
+                    fives -= 1
+                elif fives >= 3:
+                    fives -= 3
+                else:
+                    return False
+
+        return True
