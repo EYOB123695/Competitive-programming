dd93e26c06f4d5710f006eecaa0528525af65779
Eyob <eyobimpactinglife@gmail.com>
2024-03-08 20:55:04 -0800

    Add solution for Pascal's Triangle II

---
+++ campone/leetcode/pascals-triangle-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        ans = [1]
+        for i in range(rowIndex):
+            row = [0] * (len(ans)+ 1)
+            for j in range(len(ans)):
+                row[j] += ans[j]
+                row[j+1] += ans[j]
+            ans = row
+        return ans
