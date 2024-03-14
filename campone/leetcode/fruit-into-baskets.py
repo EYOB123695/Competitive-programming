53131596a25d872479b4e1a534b249031db3cee2
Eyob <eyobimpactinglife@gmail.com>
2024-03-14 04:36:51 -0700

    Add solution for Fruit Into Baskets

---
+++ campone/leetcode/fruit-into-baskets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        count = defaultdict(int)
+        l = 0 
+        ans = 0
+        total = 0 
+
+        for r in range(len(fruits)):
+            count[fruits[r]]+= 1
+            total +=1 
+            while len(count) > 2 :
+                f = fruits[l]
+                count[f] -= 1
+                total -= 1 
+                l+=1
+                if not count[f]:
+                    del count[f] 
+            ans = max(ans,total)
+        return ans
