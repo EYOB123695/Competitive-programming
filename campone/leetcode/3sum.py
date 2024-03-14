c410e96929d9d36cf2a38ff3a420f317baa557ee
Eyob <eyobimpactinglife@gmail.com>
2024-03-14 04:03:13 -0700

    Add solution for 3Sum

---
+++ campone/leetcode/3sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i-1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    r -= 1
+                    #while l < r and nums[r] == nums[r+1]:
+                        #r -= 1
+        return res
