b33e42acc17d7f89b0d1dfed7b20c6a5a5ccdf1d
Eyob <eyobimpactinglife@gmail.com>
2024-03-18 01:10:45 -0700

    Add solution for Validate Binary Search Tree

---
+++ campone/leetcode/validate-binary-search-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def check(root,left,right):
+            if not root:
+                return True  
+            if not(left < root.val < right):
+                return False
+            return  check(root.left,left,root.val) and check(root.right,root.val,right)
+        return check(root,float("-inf"),float("inf"))
+            
+        
