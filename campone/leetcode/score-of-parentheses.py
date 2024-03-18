30417935713564f92e4345effbbbb3ca11d4d7e3
Eyob <eyobimpactinglife@gmail.com>
2024-03-18 01:46:28 -0700

    Add solution for Score of Parentheses

---
+++ campone/leetcode/score-of-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack= []
+        output,mul= 0 ,0 
+        for i ,p in enumerate(s):
+            if p =="(":
+                mul+=1
+            elif p== ")":
+                mul-=1
+                if i  > 0 and s[i-1] == "(":
+                    output += 2**mul
+
+        return output
+        
