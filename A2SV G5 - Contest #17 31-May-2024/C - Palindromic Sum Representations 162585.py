e078d7f74ffd8db2ad4f83eae3c2d83a52a8ba9f
Eyob <eyobimpactinglife@gmail.com>
2024-05-31 06:28:32 +0300

    Created 'C - Palindromic Sum Representations' by A2SV Hub

---
+++ A2SV G5 - Contest #17 31-May-2024/C - Palindromic Sum Representations 162585.py
@@ -0,0 +1,23 @@
+# Problem: C - Palindromic Sum Representations - https://codeforces.com/gym/524965/problem/C
+
+dp  =[1] +  [0] * (40001)
+def sol(n):
+    mod = 10 ** 9 + 7
+    
+
+    for i in range(1 , n + 1):
+        x = str(i)
+        if x == x[::-1]:
+            for j in range(i, n + 1):
+                
+                dp[j] = (dp[j]  +  dp[j - i])% mod
+
+arr = []
+T= int(input())
+for _ in range(T):
+    x = int(input())
+    arr.append(x)
+val = max(arr)
+sol(val+ 1)
+for i in arr:
+    print(dp[i])
