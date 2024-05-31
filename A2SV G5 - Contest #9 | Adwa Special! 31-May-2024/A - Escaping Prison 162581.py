4da07dc0d9a3ac6b44d0b2ad0ab980f7a93ce92a
Eyob <eyobimpactinglife@gmail.com>
2024-05-31 06:16:34 +0300

    Created 'A - Escaping Prison' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 31-May-2024/A - Escaping Prison 162581.py
@@ -0,0 +1,34 @@
+# Problem: A - Escaping Prison - https://codeforces.com/gym/513152/problem/A
+
+def sol():
+    n ,d = map(int,input().split())
+    width =[ ]
+    height = []
+    for i in range(n):
+        w,h = map(int,input().split())
+        width.append(w)
+        height.append(h)
+    
+    for i in range(len(width)):
+        if width[i] > height[i]:
+            d-=width[i]
+        else:
+            d-=height[i]
+    
+    if d <= 0:
+        return "YES"
+    else:
+        return "NO"
+    
+
+        
+
+
+
+
+
+
+
+T = int(input())
+for _ in range(T):
+    print(sol())
