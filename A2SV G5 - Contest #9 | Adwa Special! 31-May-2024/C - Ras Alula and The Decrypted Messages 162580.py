8ac4582e604b2b6a7d7cde2059ccdb2dacdd8e9b
Eyob <eyobimpactinglife@gmail.com>
2024-05-31 06:16:20 +0300

    Created 'C - Ras Alula and The Decrypted Messages' by A2SV Hub

---
+++ A2SV G5 - Contest #9 | Adwa Special! 31-May-2024/C - Ras Alula and The Decrypted Messages 162580.py
@@ -0,0 +1,41 @@
+# Problem: C - Ras Alula and The Decrypted Messages - https://codeforces.com/gym/513152/problem/C
+
+def sol():
+    n, m = map(int,input().split())
+    s = [ord(x) for x in list(input())]
+   
+    w = [ord(x) for x in list(input())]
+ 
+    l = 0
+    
+    
+    counttwo = sum(w)  
+    
+    arr = s[:m] 
+    countone = sum(arr)  
+    if countone == counttwo:
+        return "YES"
+   
+    for r in range(m,n):
+       
+        
+        countone+=s[r] 
+        countone-=s[l]  
+        l+=1  
+        if counttwo == countone:
+            return "YES"
+            
+    return "NO" 
+        
+
+
+
+
+
+
+
+
+T = int(input())
+for _ in range(T):
+    x=sol()
+    print(x) 
