c4b47eebfd8af022a4b8211c038f5dda55d6e356
Eyob <eyobimpactinglife@gmail.com>
2024-05-31 06:14:58 +0300

    Created 'A - Large Number' by A2SV Hub

---
+++ A2SV G5 - Contest #8 31-May-2024/A - Large Number 162579.py
@@ -0,0 +1,55 @@
+# Problem: A - Large Number - https://codeforces.com/gym/511242/problem/A
+
+def sol():
+    n , d = input().split()
+    x = int(n)
+    
+    a = int(d)
+    s =input()  
+    arr = list(s)
+     
+    flag = True
+    if a ==  0:
+        arr.append(0)
+        flag = False
+    else:
+        for i in range(len(s)):  
+            if a > int(s[i]) : 
+                idx = i
+                arr.insert(idx,a) 
+                flag = False
+
+                break 
+    if flag:
+        arr.append(a)  
+
+           
+    return arr
+                   
+
+           
+        
+        
+        
+        
+             
+
+
+
+
+
+
+
+
+    
+    
+   
+
+T= int(input())
+for _ in range(T):
+   answer =  sol() 
+   for i in range(len(answer)):
+        answer[i] = str(answer[i])
+    
+   x = "".join(answer)
+   print(x)  
