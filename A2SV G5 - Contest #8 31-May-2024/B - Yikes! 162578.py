2ca695224e7d7bdcd10f580682ed4188b6188ad6
Eyob <eyobimpactinglife@gmail.com>
2024-05-31 06:11:50 +0300

    Created 'B - Yikes!' by A2SV Hub

---
+++ A2SV G5 - Contest #8 31-May-2024/B - Yikes! 162578.py
@@ -0,0 +1,25 @@
+# Problem: B - Yikes! - https://codeforces.com/gym/511242/problem/B
+
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+query = list(map(int, input().split()))
+prefixsum = [0] * (len(arr)+1)
+
+
+for i in range(1,len(prefixsum)):
+    prefixsum[i] = prefixsum[i-1] + arr[i-1]
+
+
+for i in query:
+    l = 0
+    r = n
+    while l <= r:
+        mid  = l+(r-l) // 2 
+        if prefixsum[mid] < i:
+            l = mid+1
+        else:
+            r  =mid -1
+    print(l)
+
+
