ce2fd909067fe08cf6e6dfc522dfeee23d8d6de6
Eyob <eyobimpactinglife@gmail.com>
2024-06-06 21:36:39 +0300

    Created 'Max Consecutive Ones' by A2SV Hub

---
+++ 06-Jun-2024/Max Consecutive Ones 164791.py
@@ -0,0 +1,18 @@
+# Problem: Max Consecutive Ones - https://leetcode.com/problems/max-consecutive-ones/
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_count = 0
+        count = 0
+
+        for num in nums:
+            if num == 1:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                count = 0
+
+        return max_count
+
+
+        
