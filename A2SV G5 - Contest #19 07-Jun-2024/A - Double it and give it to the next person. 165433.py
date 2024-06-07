ff62dc361901b86ccf90e317282593ffbacf940d
Eyob <eyobimpactinglife@gmail.com>
2024-06-07 18:39:36 +0300

    Created 'A - Double it and give it to the next person.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 07-Jun-2024/A - Double it and give it to the next person. 165433.py
@@ -0,0 +1,25 @@
+# Problem: A - Double it and give it to the next person. - https://codeforces.com/gym/527294/problem/A
+
+T= int(input())
+for _ in range(T):
+    s = input()
+    l = ""
+    ans = []
+    final = []
+    for i in range(len(s)):
+        for j in range(2): 
+            l += s[i]
+    r = 0
+    while r < len(l):
+        ans.append(str(l[r]))
+        r+=2
+    ans.append("".join(ans[::-1]))
+    
+    print("".join(ans))
+   
+
+
+
+
+
+   
