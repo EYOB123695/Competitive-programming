7c47f72c8856ade0f8e853632eb7b5113179abf3
Eyob <eyobimpactinglife@gmail.com>
2024-06-08 21:43:33 +0300

    Created 'B - Eba loves math.' by A2SV Hub

---
+++ A2SV G5 - Contest #19 08-Jun-2024/B - Eba loves math. 165877.py
@@ -0,0 +1,23 @@
+# Problem: B - Eba loves math. - https://codeforces.com/gym/527294/problem/B
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    val = min(arr)
+    for i in arr:
+        val  = val & i
+    print(val)
+    
+    
+    
+    
+       
+       
+    
+    
+
+    
+
+    
