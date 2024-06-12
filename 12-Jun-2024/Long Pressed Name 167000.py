bdd55471a0e66f78f2b2512b3c837671e62850ba
Eyob <eyobimpactinglife@gmail.com>
2024-06-12 11:55:39 +0300

    Created 'Long Pressed Name' by A2SV Hub

---
+++ 12-Jun-2024/Long Pressed Name 167000.py
@@ -0,0 +1,20 @@
+# Problem: Long Pressed Name - https://leetcode.com/problems/long-pressed-name/
+
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        
+        
+        i,j=0,0
+        n= len(typed)
+        m =len(name)
+        while j<n:
+            if i<m and typed[j]==name[i]:
+                j+=1
+                i+=1
+            elif j>0 and typed[j]==typed[j-1]:
+                j+=1
+            else:
+                return False
+        if i==m :
+            return True
+            
