75e7192e21ffc111cb177b1fb6d254d75b73ebd2
Eyob <eyobimpactinglife@gmail.com>
2024-06-13 09:35:36 +0300

    Created 'A - Spris' by A2SV Hub

---
+++ A2SV G5 - Contest #20 13-Jun-2024/A - Spris 167645.py
@@ -0,0 +1,18 @@
+# Problem: A - Spris - https://codeforces.com/gym/528792/problem/A
+
+a = int(input())
+b = int(input())
+c = int(input())
+arr = [a,b,c]
+flag = True
+while a > 0:
+    count = 0
+    if a * 2 <= b and a * 4 <= c:
+        count = a+a*2+a*4 
+        flag = False
+        print(count)
+        break
+    else:
+        a-=1
+if flag:
+    print(0)
