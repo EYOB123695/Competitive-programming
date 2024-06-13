39cf13b176c59f0c904331909667c7b5fe64fabd
Eyob <eyobimpactinglife@gmail.com>
2024-06-13 09:35:45 +0300

    Created 'B - BANless' by A2SV Hub

---
+++ A2SV G5 - Contest #20 13-Jun-2024/B - BANless 167646.py
@@ -0,0 +1,25 @@
+# Problem: B - BANless - https://codeforces.com/gym/528792/problem/B
+
+T= int(input())
+for _ in range(T): 
+    flag = True
+    n = int(input())
+   
+    ans = []
+    i = 1
+    j = 3*n
+    while j > i:
+
+            ans.append([i,j])
+            i+=3
+            j-=3
+           
+    print(len(ans))
+    for i,j in ans:
+        print(i,j)
+
+
+
+
+    
+
