2258b881ed62b96602a30d12e418c8b44606aab1
Eyob <eyobimpactinglife@gmail.com>
2024-06-20 18:19:01 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/A - abbccc 170680.py
@@ -0,0 +1,15 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+n = int(input())
+s = input()
+ans = []
+ans.append(s[0])
+count = 2
+i = 1
+while i < n:
+    ans.append(s[i])
+    i+=count
+    count+=1
+print("".join(ans))
+
+
