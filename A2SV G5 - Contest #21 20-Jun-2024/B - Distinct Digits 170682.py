7c26fda40bd6ee328c1cf29c4746202cd7b141e4
Eyob <eyobimpactinglife@gmail.com>
2024-06-20 18:20:36 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/B - Distinct Digits 170682.py
@@ -0,0 +1,20 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+def sol():
+    n = int(input())
+    res= ""
+    for i in range(9,0,-1):
+        if i <= n:
+            res+=str(i)
+            n-=i 
+    
+    res = res[::-1] 
+    return int(res) 
+
+
+            
+
+
+T= int(input())
+for _ in range(T):
+    print(sol())
