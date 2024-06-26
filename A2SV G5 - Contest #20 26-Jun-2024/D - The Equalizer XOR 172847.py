b40973236516c71c62f3340a0f1a645607cf7261
Eyob <eyobimpactinglife@gmail.com>
2024-06-26 13:03:36 +0300

    Created 'D - The Equalizer XOR' by A2SV Hub

---
+++ A2SV G5 - Contest #20 26-Jun-2024/D - The Equalizer XOR 172847.py
@@ -0,0 +1,35 @@
+# Problem: D - The Equalizer XOR - https://codeforces.com/gym/528792/problem/D
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    arr = list(map(int,input().split()))
+    xor = [0] * n
+    Flag = True
+    xor[0] = arr[0]
+    for i  in range(1,len(arr)):
+        xor[i] = xor[i-1] ^ arr[i]
+    if xor[n-1] == 0:
+        print("YES")
+        Flag = False
+    else:
+        for i in range(n):
+            one = xor[i]
+            if not Flag:
+                break
+            for j in range(i+1,n-1):
+                two = xor[j] ^ xor[n-1]
+                three = xor[i] ^ xor[j] 
+                if one == two == three :
+                    print("YES")
+                    Flag = False 
+                    break
+    if Flag :
+
+        print("NO") 
+
+    
+
+
+
+
