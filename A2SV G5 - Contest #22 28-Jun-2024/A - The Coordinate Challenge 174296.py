2f306f72dd1c64055c371d9fc3368199fc2e46ae
Eyob <eyobimpactinglife@gmail.com>
2024-06-28 23:13:16 +0300

    Created 'A - The Coordinate Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #22 28-Jun-2024/A - The Coordinate Challenge 174296.py
@@ -0,0 +1,24 @@
+# Problem: A - The Coordinate Challenge - https://codeforces.com/gym/531455/problem/A
+
+from collections import *
+import math 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    flag =True
+    if n == 1 :
+        print(2)
+        flag =False
+    elif n == 2 or n == 3:
+        print(1)
+        flag  = False 
+    if flag:
+        x = n / 2
+        y = n/ 3
+       
+        x= math.ceil(x)
+        y = math.ceil(y)
+        if x < y :
+            print(x)
+        else:
+            print(y) 
