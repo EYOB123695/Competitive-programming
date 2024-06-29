aa5ee03c78847f86644c6dde50cc1a39bc292ce8
Eyob <eyobimpactinglife@gmail.com>
2024-06-29 22:04:44 +0300

    Created 'D - Binyam and Kalkidan' by A2SV Hub

---
+++ A2SV G5 - Contest #22 29-Jun-2024/D - Binyam and Kalkidan 174609.py
@@ -0,0 +1,23 @@
+# Problem: D - Binyam and Kalkidan - https://codeforces.com/gym/531455/problem/D
+
+n=  int(input())
+arr= input()
+check = "2357"
+ans = []
+for i in arr:
+    if i == "4":
+        ans.extend(['3','2','2'])
+    elif i =='6':
+        ans.extend(['5','3'])
+    elif i =='8':
+        ans.extend(['7','2','2','2'])
+    elif i =='9':
+        ans.extend(['7','3','3','2'])
+    elif i in check:
+        ans.append(i)
+
+ans.sort()
+ans = ans[::-1]
+
+
+print("".join(ans)) 
