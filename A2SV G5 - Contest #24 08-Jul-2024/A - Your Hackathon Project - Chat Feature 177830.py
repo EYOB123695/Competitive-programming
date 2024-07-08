40c1348131e734ab0595231f44275e7ea98569a4
Eyob <eyobimpactinglife@gmail.com>
2024-07-08 20:55:22 +0300

    Created 'A - Your Hackathon Project - Chat Feature' by A2SV Hub

---
+++ A2SV G5 - Contest #24 08-Jul-2024/A - Your Hackathon Project - Chat Feature 177830.py
@@ -0,0 +1,19 @@
+# Problem: A - Your Hackathon Project - Chat Feature - https://codeforces.com/gym/534160/problem/A
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    s = input()
+    c = 0
+
+    for i in range(n):
+        if s[i] != ")":
+            c = 0
+        else:
+            c+=1
+    
+   
+    if c > n - c:
+        print("Yes")
+    else:
+        print("No") 
