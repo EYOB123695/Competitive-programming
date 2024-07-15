a7cb703edfbbc68f3f3c739063a7d8d6e8eba1d6
Eyob <eyobimpactinglife@gmail.com>
2024-07-15 23:16:17 +0300

    Created 'Honest Coach' by A2SV Hub

---
+++ 15-Jul-2024/Honest Coach 180696.py
@@ -0,0 +1,25 @@
+# Problem: Honest Coach - https://codeforces.com/problemset/problem/1360/B
+
+import math
+T = int(input())
+arr = []
+for _ in range(T):
+    n = int(input())
+    arr = list(map(int,input().split()))
+   
+    
+    arr.sort() 
+    ans = []
+    diff = float("inf")
+
+    for i in range(len(arr)-1):
+         
+        diff = min(diff,arr[i+1] -arr[i])
+    print(diff)
+
+
+
+           
+
+
+ 
