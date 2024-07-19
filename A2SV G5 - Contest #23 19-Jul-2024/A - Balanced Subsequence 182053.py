da850d5b25188a254232b4038f97b8b2fbeec791
Eyob <eyobimpactinglife@gmail.com>
2024-07-20 00:22:41 +0300

    Created 'A - Balanced Subsequence' by A2SV Hub

---
+++ A2SV G5 - Contest #23 19-Jul-2024/A - Balanced Subsequence 182053.py
@@ -0,0 +1,53 @@
+# Problem: A - Balanced Subsequence - https://codeforces.com/gym/532814/problem/A
+
+from collections import *
+n,k = map(int,input().split())
+s =  input()
+
+x = []
+flag = True
+dic = defaultdict(int)
+s = s.lower()
+for i in range(len(s)):
+     dic[s[i]] += 1
+
+
+
+
+for i in range(n): 
+    c =  ord(s[i]) - ord("a")
+    if c > k:
+         print(0)
+         flag = False
+    
+    x.append(c)
+
+
+for i in range(k) :
+    
+    if i not in x:
+        flag = False 
+        print(0)
+        break
+
+if flag:
+    e = float("inf")
+    for key , val in dic.items():
+        
+           b = ord(key)   - ord("a")
+         
+           if b in x:
+                e = min(e,val)
+  
+    ans = 0
+    for key, val in dic.items(): 
+        b = ord(key)   - ord("a")
+        if  b in x:  
+             ans += e 
+    
+  
+    print(ans) 
+       
+
+
+
