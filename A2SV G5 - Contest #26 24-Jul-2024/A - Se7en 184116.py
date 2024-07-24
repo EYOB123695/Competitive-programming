dcf9c0cb53b92fd67d66bc085a03b691ed931a72
Eyob <eyobimpactinglife@gmail.com>
2024-07-24 13:44:14 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 24-Jul-2024/A - Se7en 184116.py
@@ -0,0 +1,16 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+import math
+T = int(input())
+for _ in range(T):
+    x = int(input())
+    if x % 7 == 0:
+        print(x)
+    else:
+        x-= x %10
+        while x %7:
+            x += 1 
+        print(x)        
+            
+            
+      
