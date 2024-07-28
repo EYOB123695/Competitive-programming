bfb773b050d39acc29665fa6d13152f3b67cde81
Eyob <eyobimpactinglife@gmail.com>
2024-07-29 00:29:45 +0300

    Created 'Valid Parentheses' by A2SV Hub

---
+++ 28-Jul-2024/Valid Parentheses 185535.py
@@ -0,0 +1,22 @@
+# Problem: Valid Parentheses - https://leetcode.com/problems/valid-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for i in range(len(s)):
+            if s[i] == '{' or s[i] == "[" or s[i]== "(":
+                stack.append(s[i])
+            if s[i] == "}" or s[i]==")" or s[i] == "]": 
+                if len(stack) == 0 :
+                    return False
+                
+                if  (s[i] == "}" and  stack[-1] != "{") or (s[i] == "]" and stack[-1] != "[" ) or (s[i] == ")" and stack[-1] != "("):
+                    return False
+                stack.pop()
+
+        if  len(stack) == 0:
+            return True
+        else:
+            return False
+        
+        
