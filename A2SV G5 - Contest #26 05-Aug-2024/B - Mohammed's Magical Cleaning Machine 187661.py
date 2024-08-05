0b74991dc9257f10ed1a26ed3214de6f5c83243f
Eyob <eyobimpactinglife@gmail.com>
2024-08-05 22:09:17 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 05-Aug-2024/B - Mohammed's Magical Cleaning Machine 187661.py
@@ -0,0 +1,12 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+for _ in range(int(input())):
+    n = int(input())
+    arr= list(map(int, input().split()))
+
+    ans = 0 
+    for a in arr[:-1]:
+        if a or ans > a:
+            ans += a + int(a == 0)
+
+    print(ans)
