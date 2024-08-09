0b3c0e27cb21a4502724d5e6d69a5384ba161688
Eyob <eyobimpactinglife@gmail.com>
2024-08-09 23:57:39 +0300

    Created 'B - K-divisible Sum' by A2SV Hub

---
+++ A2SV G5 - Contest #28 09-Aug-2024/B - K-divisible Sum 190206.py
@@ -0,0 +1,14 @@
+# Problem: B - K-divisible Sum - https://codeforces.com/gym/540354/problem/B
+
+ 
+from collections import *
+import math
+T= int(input())
+ 
+for _  in range(T):
+    n,k = map(int,input().split())
+    if n>= k:
+        print(1 if n % k == 0 else 2)
+    else :
+        k += (n - (n % k))
+        print(math.ceil(k / n))
