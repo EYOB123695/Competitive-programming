4c818eeafdefd5635f3e9dc842a9402e0c109568
Eyob <eyobimpactinglife@gmail.com>
2024-08-09 23:09:17 +0300

    Created 'A - Collecting Coins' by A2SV Hub

---
+++ A2SV G5 - Contest #28 09-Aug-2024/A - Collecting Coins 190167.py
@@ -0,0 +1,24 @@
+# Problem: A - Collecting Coins - https://codeforces.com/gym/540354/problem/A
+
+T = int(input())
+for _ in range(T):
+    a,b,c,p = map(int,input().split())
+    val = max(a,b,c)
+    if a != val :
+       
+        p-= (val - a )
+        a += (val -a )
+    if b != val : 
+        
+        p-= (val - b )
+        b += (val -b )
+    if c != val:
+        
+        p-= (val - c )
+        c += (val -c )
+        
+    if p < 0 or p % 3 != 0:
+        print("NO")
+    else:
+        print("YES")
+    
