f30617030b7e8d051c29a8e263cb26596055804d
Eyob <eyobimpactinglife@gmail.com>
2024-08-10 21:30:19 +0300

    Created 'B - Optimal Point' by A2SV Hub

---
+++ A2SV G5 - Contest #25 10-Aug-2024/B - Optimal Point 190352.py
@@ -0,0 +1,18 @@
+# Problem: B - Optimal Point - https://codeforces.com/gym/535749/problem/B
+
+T= int(input())
+for _ in range(T):
+    n,k = map(int,input().split())
+    checkone  = False
+    checktwo = False
+    for i in range(n):
+        
+        l,r = map(int,input().split())
+        if l == k:
+            checkone = True 
+        if r == k :
+            checktwo =True 
+    if checktwo and checkone:
+        print("YES")
+    else:
+        print("NO")
