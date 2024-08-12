9780cf0b905599facaef52be69097b8dca8b858b
Eyob <eyobimpactinglife@gmail.com>
2024-08-12 13:25:19 +0300

    Created 'Can I Square?' by A2SV Hub

---
+++ 12-Aug-2024/Can I Square? 190866.py
@@ -0,0 +1,13 @@
+# Problem: Can I Square? - https://codeforces.com/contest/1915/problem/C
+
+T = int(input())
+for i in range(T):
+    n= int(input())
+    arr = list(map(int,input().split()))
+    x = sum(arr)
+    y = x ** 0.5 
+  
+    if y.is_integer():
+        print("YES")
+    else:
+        print("NO")
