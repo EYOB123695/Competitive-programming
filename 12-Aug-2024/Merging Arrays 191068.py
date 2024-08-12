cc941212ad69c1a3a352334139df9994f7ca1fe5
Eyob <eyobimpactinglife@gmail.com>
2024-08-12 23:51:41 +0300

    Created 'Merging Arrays' by A2SV Hub

---
+++ 12-Aug-2024/Merging Arrays 191068.py
@@ -0,0 +1,24 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+n, m = map(int,input().split()) 
+arr = list(map(int,input().split()))
+arrtwo = list(map(int,input().split()))
+l = 0 
+r = 0 
+ans = []
+while l < len(arr) and r < len(arrtwo) :
+    if arr[l] <= arrtwo[r] :
+        ans.append(arr[l])
+        l+=1 
+    else :
+        ans.append(arrtwo[r])
+        r+=1 
+while l != len(arr) :
+    ans.append(arr[l])
+    l+=1
+while r != len(arrtwo) :
+    ans.append(arrtwo[r])
+    r+=1
+
+res =" ".join(str(i) for i in ans) 
+print(res)
