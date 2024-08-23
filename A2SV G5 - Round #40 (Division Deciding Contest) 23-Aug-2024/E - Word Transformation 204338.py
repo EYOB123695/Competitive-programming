35d9f8318011cf7b841b2292c8a9415f52b910a1
Eyob <eyobimpactinglife@gmail.com>
2024-08-23 22:52:14 +0300

    Created 'E - Word Transformation' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 23-Aug-2024/E - Word Transformation 204338.py
@@ -0,0 +1,46 @@
+# Problem: E - Word Transformation - https://codeforces.com/gym/543431/problem/E
+
+from collections import *
+n = int(input())
+dictionary = defaultdict(list)
+for _ in range(n):
+    flag  = True
+    s,t  = input().split()
+    dictionary = Counter(s)
+    dictionarytwo = Counter(t)
+    removecount = {}
+    ans = []
+    for i in s:
+        removecount[i] = dictionary[i] -  dictionarytwo[i]
+        if removecount[i] < 0:
+            flag =False 
+    if not flag:
+        print("NO")
+        continue 
+    for i in s:
+        if removecount[i] > 0 :
+            removecount[i] -= 1
+        else:
+            ans.append(i)
+    if "".join(ans) != t:
+        print("NO")
+    else:
+        print("YES") 
+    
+     
+
+    
+
+     
+    
+
+    
+
+
+
+
+
+      
+
+
+
