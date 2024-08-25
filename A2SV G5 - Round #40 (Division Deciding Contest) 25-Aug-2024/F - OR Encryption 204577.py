a66c2f310bd57b4eea4062b5587683844ad92081
Eyob <eyobimpactinglife@gmail.com>
2024-08-25 05:09:24 +0300

    Created 'F - OR Encryption' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 25-Aug-2024/F - OR Encryption 204577.py
@@ -0,0 +1,30 @@
+# Problem: F - OR Encryption - https://codeforces.com/gym/543431/problem/F
+
+def sol():
+    n = int(input())
+    matrix = []
+    
+    for j in range(n):
+        row = list(map(int,input().split()))
+        matrix.append(row)
+    arr = [2**30 -1 ] * n
+    
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                arr[i] &= matrix[i][j]
+                arr[j] &= matrix[i][j]
+    for i in range(n):
+        for j in range(n):
+            if i != j and arr[i] | arr[j] != matrix[i][j]:
+                print ("NO")
+                return
+            
+            
+    print("YES")
+    print(*arr)
+
+T = int(input())
+for  _ in range(T):
+    sol()
+  
