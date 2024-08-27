6fbff7ef713bcbbf624faa6bb4222865f6e87f6c
Eyob <eyobimpactinglife@gmail.com>
2024-08-27 21:37:24 +0300

    Created 'C - Avoid Trygub' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 27-Aug-2024/C - Avoid Trygub 208372.py
@@ -0,0 +1,34 @@
+# Problem: C - Avoid Trygub - https://codeforces.com/gym/543431/problem/C
+
+from collections import *
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    s = input()
+    x = list(s)
+   
+   
+    stack = ["b","u","g","y","r","t"] 
+   
+    for idx,i in enumerate(x):
+        
+        if stack and i == stack[-1]:
+           
+           
+            val = stack.pop()
+          
+      
+ 
+    
+   
+    if len(stack) != 0:
+        print(s)
+        
+    else:
+        for i in range(len(x)):
+            for j in range(i,len(x)):
+                if x[i] == "t" and x[j] == "b":
+                    x[i] , x[j] = x[j] , x[i] 
+                
+     
+        print("".join(x)) 
