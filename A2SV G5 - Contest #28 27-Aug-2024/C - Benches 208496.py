83773f6961590c0c5f09ee054e6042891fa83644
Eyob <eyobimpactinglife@gmail.com>
2024-08-27 23:15:17 +0300

    Created 'C - Benches' by A2SV Hub

---
+++ A2SV G5 - Contest #28 27-Aug-2024/C - Benches 208496.py
@@ -0,0 +1,19 @@
+# Problem: C - Benches - https://codeforces.com/gym/540354/problem/C
+
+from heapq import *
+n = int(input())
+m = int(input())
+arr = [ ]
+for i in range(n):
+
+    arr.append(int(input()))
+anstwo =  max(arr) + m
+heapify(arr)
+for i in range(m):
+    x = heappop(arr)
+    heappush(arr,x+1)
+ansone = max(arr)
+print(ansone,anstwo , sep =" ")
+
+
+
