93c3118e9a3f391cdda6e0bf369e3c63efb24ddd
Eyob <eyobimpactinglife@gmail.com>
2024-09-01 12:16:55 +0300

    Created 'C - Permutation Sorting' by A2SV Hub

---
+++ A2SV G5 - Contest #27 01-Sep-2024/C - Permutation Sorting 210077.py
@@ -0,0 +1,15 @@
+# Problem: C - Permutation Sorting - https://codeforces.com/gym/538762/problem/C
+
+T= int(input())
+for _ in range(T):
+    n= int(input())
+    arr = list(map(int,input().split()))
+    ans= 0 
+    x = set()
+    for i in arr:
+        if i+1 in x:
+            ans += 1
+        x.add(i)
+    print(ans)
+
+
