06c5297a234d1ee39a2d17e704229a2b711d712c
Eyob <eyobimpactinglife@gmail.com>
2024-09-01 13:05:36 +0300

    Created 'D - Candies in the Box' by A2SV Hub

---
+++ A2SV G5 - Contest #27 01-Sep-2024/D - Candies in the Box 210087.py
@@ -0,0 +1,29 @@
+# Problem: D - Candies in the Box - https://codeforces.com/gym/538762/problem/D
+
+n = int(input())
+l = 1 
+r = n 
+def check(mid,n):
+    
+    count = 0
+    
+    curr = n
+    while curr > 0:
+        mid = min(curr,mid)
+        curr -= mid
+        count += mid
+        curr -= curr // 10
+       
+
+
+    return count * 2 >= n
+ans =  n
+while l<= r :
+    mid = l + (r-l)//2 
+    if check(mid,n):
+        ans = mid
+        r = mid -1 
+    else:
+        l = mid +1 
+print(ans)
+    
