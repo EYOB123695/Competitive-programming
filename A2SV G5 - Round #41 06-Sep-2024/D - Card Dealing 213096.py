589262ad14b79751046edd334a1900d4e74be812
Eyob <eyobimpactinglife@gmail.com>
2024-09-06 21:51:16 +0300

    Created 'D - Card Dealing' by A2SV Hub

---
+++ A2SV G5 - Round #41 06-Sep-2024/D - Card Dealing 213096.py
@@ -0,0 +1,41 @@
+# Problem: D - Card Dealing - https://codeforces.com/gym/545013/problem/D
+
+from collections import *
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    alice = 1
+    bob = 0
+    flag = False
+    countbob = 0
+    countalice = 0
+    res = []
+    ava = n-1
+    for i in range(2,n+1):
+        if flag :
+            if  ava <= i: 
+                alice += ava
+                break 
+
+            ava-=i
+            alice +=  i
+            countalice +=1 
+            if countalice == 2 :
+                flag = False
+                countalice = 0
+        elif not flag :
+            if  ava <= i: 
+                bob += ava
+                break 
+            ava-=i  
+            bob += i
+            countbob += 1
+            if countbob == 2 :
+                flag = True
+                countbob = 0
+           
+ 
+    print(alice , bob , sep = " ")
+
+
+        
