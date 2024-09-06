51b7463307850a7871726b7574eb7d11cd5a6c97
Eyob <eyobimpactinglife@gmail.com>
2024-09-06 21:51:37 +0300

    Created 'E - Min Divisible Outside Segment' by A2SV Hub

---
+++ A2SV G5 - Round #41 06-Sep-2024/E - Min Divisible Outside Segment 213098.py
@@ -0,0 +1,14 @@
+# Problem: E - Min Divisible Outside Segment - https://codeforces.com/gym/545013/problem/E
+
+q=  int(input())
+for i in range(q):
+    l,r,d = map(int,input().split())
+    if d < l :
+        print(d)
+    else:
+        if r % d == 0:
+            print(r + d)
+        else :
+           print( r + (d - r%d))
+    
+
