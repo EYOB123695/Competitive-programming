a5e8a2ba77e4fae3313aa8e57f5876ea3d9d31ec
Eyob <eyobimpactinglife@gmail.com>
2024-09-19 00:39:27 +0300

    Created 'C - Selecting Theatre Troupe' by A2SV Hub

---
+++ A2SV G5 - Contest #25 18-Sep-2024/C - Selecting Theatre Troupe 215844.py
@@ -0,0 +1,13 @@
+# Problem: C - Selecting Theatre Troupe - https://codeforces.com/gym/535749/problem/C
+
+
+
+from math import comb
+
+n, m, t = map(int,input().split())
+
+res = 0
+for b in range(4, t):
+    res += comb(n, b)  * comb(m, t  - b)
+
+print(res)
