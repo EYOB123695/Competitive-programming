7574dd85df2c1ba1f1be1cf75792a68b987e1671
Eyob <eyobimpactinglife@gmail.com>
2024-09-19 23:36:05 +0300

    Created 'Heap Operations' by A2SV Hub

---
+++ 19-Sep-2024/Heap Operations 215968.py
@@ -0,0 +1,44 @@
+# Problem: Heap Operations - https://codeforces.com/contest/681/problem/C
+
+from collections import * 
+import heapq
+n = int(input())
+arr = []
+for i in range(n):
+    x = input()
+    arr.append(x)
+x = []
+heapq.heapify(x)
+ans = []
+for i in arr:
+    if  i[0] == "i" :
+        
+        h,y = i.split()
+        y  = int(y)
+        heapq.heappush(x,y)
+        ans.append(i)
+
+    elif i[0] == "g":
+        z,k = i.split()
+        k = int(k)
+        while x and x[0] <k:
+            heapq.heappop(x)
+            ans.append("removeMin")
+        if not x or x[0] > k:
+            heapq.heappush(x,k) 
+            ans.append(f"insert {k}")
+        ans.append(i)
+        
+    else:
+        
+        if not x:
+            ans.append("insert 0")
+            heapq.heappush(x,0) 
+
+        heapq.heappop(x)
+        ans.append(i) 
+print(len(ans))
+for j in ans:
+    print(j)
+
+
