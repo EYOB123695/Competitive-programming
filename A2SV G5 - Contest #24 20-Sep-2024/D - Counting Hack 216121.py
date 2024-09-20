913d34c615942f1b2f0aa9a07b2d14678b56ecc2
Eyob <eyobimpactinglife@gmail.com>
2024-09-20 23:09:16 +0300

    Created 'D - Counting Hack' by A2SV Hub

---
+++ A2SV G5 - Contest #24 20-Sep-2024/D - Counting Hack 216121.py
@@ -0,0 +1,25 @@
+# Problem: D - Counting Hack - https://codeforces.com/gym/534160/problem/D
+
+for _ in range(int(input())):
+    length = int(input())
+    numbers = list(map(int, input().split()))
+
+    seen = set()
+    suffix_count = [0] * (length + 1)
+    for idx in range(length - 1, -1, -1):
+        if numbers[idx] not in seen:
+            seen.add(numbers[idx])
+            suffix_count[idx + 1] = 1
+            seen.add(numbers[idx])
+    
+    for idx in range(1, len(suffix_count)):
+        suffix_count[idx] += suffix_count[idx - 1]
+
+    seen.clear()
+    result = 0
+    for idx in range(length):
+        if numbers[idx] not in seen:
+            result += suffix_count[-1] - suffix_count[idx]
+            seen.add(numbers[idx])
+
+    print(result)
