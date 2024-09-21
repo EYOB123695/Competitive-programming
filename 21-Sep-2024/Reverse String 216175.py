4b42611c79e8691cc0c0f577703f0fc70eb21bfc
Eyob <eyobimpactinglife@gmail.com>
2024-09-21 11:02:01 +0300

    Created 'Reverse String' by A2SV Hub

---
+++ 21-Sep-2024/Reverse String 216175.py
@@ -0,0 +1,16 @@
+# Problem: Reverse String - https://leetcode.com/problems/reverse-string/description/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s)-1 
+        while l< r :
+            s[l],s[r] = s[r],s[l]
+            l+=1 
+            r-=1 
+         
+
+        
