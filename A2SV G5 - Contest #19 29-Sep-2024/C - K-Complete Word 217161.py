6d6ad9836907b419b4b9f4a202d2979524573a42
Eyob <eyobimpactinglife@gmail.com>
2024-09-30 00:06:22 +0300

    Created 'C - K-Complete Word' by A2SV Hub

---
+++ A2SV G5 - Contest #19 29-Sep-2024/C - K-Complete Word 217161.py
@@ -0,0 +1,30 @@
+# Problem: C - K-Complete Word - https://codeforces.com/gym/527294/problem/C
+
+
+from collections import defaultdict, Counter
+
+def sol():
+    for _ in range(int(input())):
+        n, k = map(int, input().split())
+        s = input()
+        
+        freq = defaultdict(Counter)
+        for i in range(n):
+            loc = min(i % k, k - i % k - 1)
+            freq[loc][s[i]] += 1
+        
+        changes_needed = 0
+        tot = 2 * (n // k)
+        for i in range(k // 2):
+            max_freq= max(freq[i].values())
+            changes_needed += tot - max_freq
+        
+        if k % 2:
+            max_freq = max(freq[k//2].values())
+            changes_needed += n//k - max_freq
+        
+        print(changes_needed)
+
+
+
+sol()
