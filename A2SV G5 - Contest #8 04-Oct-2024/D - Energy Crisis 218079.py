29a61307d80a187ef36eacb75bf3fdc0e8bf02c5
Eyob <eyobimpactinglife@gmail.com>
2024-10-04 21:54:08 +0300

    Created 'D - Energy Crisis' by A2SV Hub

---
+++ A2SV G5 - Contest #8 04-Oct-2024/D - Energy Crisis 218079.py
@@ -0,0 +1,27 @@
+# Problem: D - Energy Crisis - https://codeforces.com/gym/511242/problem/D
+
+import sys
+n, k = map(int, sys.stdin.readline().strip().split())
+nums = list(map(int, sys.stdin.readline().strip().split()))
+
+def chekcker(x):
+    gain = 0
+    need = 0
+    for num in nums:
+        if num > x:
+            diff = num - x
+            gain += (diff - diff * k / 100)
+        if num < x:
+            need  += (x - num)
+    
+    return gain >= need
+
+low = min(nums)
+high = max(nums)
+while high  - low > 10 ** -7:
+    mid = (high + low) / 2
+    if chekcker(mid):
+        low = mid
+    else:
+        high = mid
+print(high)
