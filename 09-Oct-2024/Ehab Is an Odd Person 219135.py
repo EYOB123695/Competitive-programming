70293d38a71c9b4bd6ac828ce110777594efb2ad
Eyob <eyobimpactinglife@gmail.com>
2024-10-09 23:34:16 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 09-Oct-2024/Ehab Is an Odd Person 219135.py
@@ -0,0 +1,22 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+from collections import * 
+n = int(input())
+arr = list(map(int,input().split()))
+countodd = 0
+counteven = 0 
+for i in arr:
+    if i % 2 == 0:
+        counteven += 1
+ 
+    else :
+        countodd += 1 
+ 
+if counteven != 0 and countodd != 0:
+    arr.sort()
+    
+    print(" ".join(map(str,arr)))
+ 
+else:
+ 
+    print(" ".join(map(str,arr)))
