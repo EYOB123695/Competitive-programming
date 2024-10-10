51409e766a54b15ea19905bbc3cc6ddf1fd37faf
Eyob <eyobimpactinglife@gmail.com>
2024-10-10 15:02:30 +0300

    Created 'Path with Maximum Probability' by A2SV Hub

---
+++ 10-Oct-2024/Path with Maximum Probability 219211.py
@@ -0,0 +1,23 @@
+# Problem: Path with Maximum Probability - https://leetcode.com/problems/path-with-maximum-probability/
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        dist = [0] * n
+        dist[start_node] = 1
+        
+        for _ in range(n - 1):
+            updated = False
+            for i, (u, v) in enumerate(edges):
+                if dist[u] * succProb[i] > dist[v]:
+                    dist[v] = dist[u] * succProb[i]
+                    updated = True
+                if dist[v] * succProb[i] > dist[u]:
+                    dist[u] = dist[v] * succProb[i]
+                    updated = True
+            if not updated:
+                break
+        
+        return dist[end_node]
+
+
+        
