88b1652358eadf3773d0ce81b8c1b7580e1583ef
Eyob <eyobimpactinglife@gmail.com>
2024-10-24 00:09:52 +0300

    Created 'F - Swapping' by A2SV Hub

---
+++ A2SV G5 - Round #41 23-Oct-2024/F - Swapping 221892.py
@@ -0,0 +1,14 @@
+# Problem: F - Swapping - https://codeforces.com/gym/544853/problem/D
+
+t = int(input())
+for _ in range(t):
+    n, k, m = list(map(int, input().split()))
+    start = k
+    end = k
+    
+    for i in range(m):
+        l, r = map(int, input().split()) 
+        if l <= start <= r or l <= end <= r:
+            start = min(start, l)
+            end = max(end, r)
+    print(end - start + 1)
