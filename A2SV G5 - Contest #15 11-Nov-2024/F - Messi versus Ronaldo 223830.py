e60ef24119e91414bed5fac3e295ac816b8f47e9
Eyob <eyobimpactinglife@gmail.com>
2024-11-11 23:41:21 +0300

    Created 'F - Messi versus Ronaldo' by A2SV Hub

---
+++ A2SV G5 - Contest #15 11-Nov-2024/F - Messi versus Ronaldo 223830.py
@@ -0,0 +1,31 @@
+# Problem: F - Messi versus Ronaldo - https://codeforces.com/gym/522079/problem/F
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    MOD = 1000_000_007
+    bit_counts = [0]*60
+    for j in arr:
+        for i in range(60):
+            if j&(1<<i):
+                bit_counts[i] += 1
+    final = 0
+    for j in arr:
+        totalone = 0
+        totaltwo = 0
+        for i in range(60):
+            if j&(1<<i):
+                totalone += (1<<i)%MOD*n
+                totalone %= MOD
+                totaltwo += (1<<i)%MOD*bit_counts[i]
+                totaltwo %= MOD
+            else:
+                totalone += (1<<i)%MOD*bit_counts[i]
+                totalone %= MOD
+        final += totalone*totaltwo
+        final %= MOD
+
+    print(final)
