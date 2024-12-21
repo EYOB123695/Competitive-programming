ab508a8344b94147a228bcd4a4ca6c46f5fff02f
Eyob <eyobimpactinglife@gmail.com>
2024-12-21 11:33:46 +0300

    Created 'Dota2 Senate' by A2SV Hub

---
+++ 21-Dec-2024/Dota2 Senate 224912.py
@@ -0,0 +1,28 @@
+# Problem: Dota2 Senate - https://leetcode.com/problems/dota2-senate/
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        senate = list(senate)
+        d, r = deque() , deque()
+        for i, val in enumerate(senate):
+            if  val == "R":
+                r.append(i)
+            else :
+                d.append(i) 
+        while d and r :
+            left = d.popleft()
+            right = r.popleft()
+            if left < right :
+                d.append(left + n ) 
+            else:
+                r.append(right + n) 
+        if r :
+            return "Radiant" 
+        else :
+           return  "Dire"
+
+
+
+
+        
