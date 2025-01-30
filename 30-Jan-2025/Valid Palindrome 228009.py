ea43822b660eb3a8d62b41fe8ed20a914bcd319a
Eyob <eyobimpactinglife@gmail.com>
2025-01-30 20:56:06 +0300

    Created 'Valid Palindrome' by A2SV Hub

---
+++ 30-Jan-2025/Valid Palindrome 228009.py
@@ -0,0 +1,24 @@
+# Problem: Valid Palindrome - https://leetcode.com/problems/valid-palindrome/description/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l += 1
+                r -= 1
+
+        return True
+
+ 
+        
+
+        
