2f55d5e26e7ee57099ca4942d3f33a3990e74862
Eyob <eyobimpactinglife@gmail.com>
2025-02-27 23:36:56 +0300

    Created 'Subsets' by A2SV Hub

---
+++ 27-Feb-2025/Subsets 286512.py
@@ -0,0 +1,29 @@
+# Problem: Subsets - https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        bucket = []
+        ans = []
+        def backtrack(i):
+            if i >= len(nums):
+                ans.append(bucket.copy())
+                
+                return 
+            bucket.append(nums[i])
+            
+            
+            backtrack(i+1) 
+            bucket.pop()
+            backtrack(i+1)
+        backtrack(0)
+        return ans
+
+            
+        
+            
+            
+
+
+
+
+        
