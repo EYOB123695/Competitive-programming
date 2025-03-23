bea3a440273325a7c1ef72584a12f1899beb898a
Eyob <eyobimpactinglife@gmail.com>
2025-03-23 21:51:17 +0300

    Created 'Reverse Bits' by A2SV Hub

---
+++ 23-Mar-2025/Reverse Bits 316254.py
@@ -0,0 +1,16 @@
+# Problem: Reverse Bits - https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        
+        for i in range(32):
+            if n & (1<<i):
+                res |= (1<<(31 -i)) 
+        return res
+
+
+
+
+        
+        
