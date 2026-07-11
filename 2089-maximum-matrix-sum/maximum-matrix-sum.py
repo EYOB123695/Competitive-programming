0368a1678da48334ab8ef08363c54c2532c75afb
Eyob <eyobimpactinglife@gmail.com>
2026-07-11 22:34:19 +0300

    Time: 99 ms (5.03%) | Memory: 29 MB (25.95%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        sum = 0 
+        count = 0
+        val = float("inf")
+        flag = False
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                sum += abs(matrix[i][j])
+                if matrix[i][j] < 0 :
+                    count  += 1 
+                
+                elif matrix[i][j] == 0 :
+                    flag = True
+                val = min(val,abs(matrix[i][j]))
+                    
+                        
+
+        print(val)
+        if count % 2 == 0 or flag :
+            return sum 
+        else :
+            return sum + (2 * ( - val)) 
+
+        
+
+
+        
