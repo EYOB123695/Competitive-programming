bf43e41f15d783edf6a41f123c5b40a52e9977d3
Eyob <eyobimpactinglife@gmail.com>
2026-08-31 13:22:47 +0300

    Time: 15 ms (43.65%) | Memory: 19.3 MB (76.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dictone = Counter(s)
-        dicttwo = Counter(t) 
-        if dictone == dicttwo :
-            return True 
-        else : 
-            return False  
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for char_s ,char_t in zip(s,t):
+            count[ord (char_s)-ord("a")] += 1 
+            count[ord (char_t)-ord("a")] -= 1
+        return all (c == 0 for c in count)  
+
+      
 
         
